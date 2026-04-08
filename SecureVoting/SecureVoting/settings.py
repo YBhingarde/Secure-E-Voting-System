ffcdc0dbbc21885bbeff090860ca5079fb209907
Yash Bhingarde <yashbhingarde3@gmail.com>
2026-04-09 00:32:50 +0530

    Email setting update

--- SecureVoting/SecureVoting/settings.py
+++ SecureVoting/SecureVoting/settings.py
@@ -131,7 +131,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Email Settings 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yashbhingarde21@gmail.com' 
-EMAIL_HOST_PASSWORD = 'qmrwfiiajjxfysox' 
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True  
+EMAIL_HOST_USER = 'yashbhingarde21@gmail.com'
+EMAIL_HOST_PASSWORD = 'qmrwfiiajjxfysox'
+DEFAULT_FROM_EMAIL = 'yashbhingarde21@gmail.com'
